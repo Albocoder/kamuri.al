69cd70c04066a9cd64383df67238a81808d18794
Albocoder <albocoder@gmail.com>
2016-02-09 19:20:14 +0200

    Fixed database query issue. Added a non-working login!

--- kamuriTBL.sql
+++ kamuriTBL.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: sql2.freemysqlhosting.net
--- Generation Time: Feb 08, 2016 at 03:22 PM
+-- Generation Time: Feb 09, 2016 at 03:09 PM
 -- Server version: 5.5.46-0ubuntu0.12.04.2
 -- PHP Version: 5.3.28
 
@@ -28,14 +28,14 @@ SET time_zone = "+02:00";
 
 CREATE TABLE IF NOT EXISTS `kamuriTBL` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) NOT NULL,
-  `pw` varchar(50) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `profpic` varchar(100) NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `pw` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `profpic` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `secQ` int(11) NOT NULL,
-  `secAns` varchar(50) NOT NULL,
-  `fbToken` varchar(50) NOT NULL,
-  `gpToken` varchar(50) NOT NULL DEFAULT '0',
+  `secAns` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fbToken` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `gpToken` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
   `role` int(11) NOT NULL DEFAULT '8',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`,`profpic`),
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS `kamuriTBL` (
 --
 
 INSERT INTO `kamuriTBL` (`id`, `email`, `pw`, `address`, `profpic`, `secQ`, `secAns`, `fbToken`, `gpToken`, `role`) VALUES
-(1, 'albocoder@gmail.com', '422e0ec76fbabc62913bbde3628f9ba7c2f32ba', 'Elbasan,Albania', 'user.jpg', 1, 'bacon', '0', '0', 1),
+(1, 'albocoder@gmail.com', 'erin', 'Elbasan,Albania', 'user.jpg', 1, 'bacon', '0', '0', 1),
 (2, 'argertboja@yahoo.com', '422e0ec76fbabc62913bbde3628f9ba7c2f32ba', 'Elbasan,Albania', 'defaultPic.jpg', 0, '', '', '0', 8);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
