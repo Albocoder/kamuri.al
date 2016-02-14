731c284c9d124149af74e4ef875e29c9069385dc
Albocoder <albocoder@gmail.com>
2016-02-14 22:06:32 +0200

    I shtova 'salt'me 50 karaktere tabeles. Per ta bere encryptionin e passwordit me te sigurte.

--- kamuriTBL.sql
+++ kamuriTBL.sql
@@ -3,12 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: sql2.freemysqlhosting.net
--- Generation Time: Feb 09, 2016 at 03:09 PM
+-- Generation Time: Feb 14, 2016 at 07:56 PM
 -- Server version: 5.5.46-0ubuntu0.12.04.2
 -- PHP Version: 5.3.28
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+02:00";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -29,7 +29,8 @@ SET time_zone = "+02:00";
 CREATE TABLE IF NOT EXISTS `kamuriTBL` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `pw` varchar(101) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `pw` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `kryp` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `address` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `profpic` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `secQ` int(11) NOT NULL,
@@ -46,9 +47,9 @@ CREATE TABLE IF NOT EXISTS `kamuriTBL` (
 -- Dumping data for table `kamuriTBL`
 --
 
-INSERT INTO `kamuriTBL` (`id`, `email`, `pw`, `address`, `profpic`, `secQ`, `secAns`, `fbToken`, `gpToken`, `role`) VALUES
-(1, 'albocoder@gmail.com', 'erin', 'Elbasan,Albania', 'user.jpg', 1, 'bacon', '0', '0', 1),
-(2, 'argertboja@yahoo.com', '422e0ec76fbabc62913bbde3628f9ba7c2f32ba', 'Elbasan,Albania', 'defaultPic.jpg', 0, '', '', '0', 8);
+INSERT INTO `kamuriTBL` (`id`, `email`, `pw`, `kryp`, `address`, `profpic`, `secQ`, `secAns`, `fbToken`, `gpToken`, `role`) VALUES
+(1, 'albocoder@gmail.com', 'db2d5c79e77af71cfdd2fbcb6a4a9a569c3a3435c871643f65525646a837a283afa2811d2d5905d70b2c154eeed8c9471646', '', 'Elbasan,Albania', 'user.jpg', 1, 'bacon', '0', '0', 1),
+(2, 'argertboja@yahoo.com', '0331f7ccb4563faf9e42cc9a1380e42095602ed48a1aedf493333cf5521386ef09e2304018fc4e18f7d5334379632b577ac0', '', 'Elbasan,Albania', 'defaultPic.jpg', 1, 'pizza', '0', '0', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
