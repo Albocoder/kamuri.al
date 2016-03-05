7bd3ebc724ee5f3390a739e3be6f7a525f2b2e42
Albocoder <albocoder@gmail.com>
2016-03-05 02:51:06 +0200

    Secure Mailer to Admins

--- html/contactMe.php
+++ html/contactMe.php
@@ -1,22 +1,37 @@
 <!DOCTYPE Html>
 <html>
 <body>
-	<b>Status:</b> 
+	<b>Status:</b>
 	<?php
 		if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['msg'])){
 			if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['msg'])){
 				$name = $_POST['name'];
 				$email = $_POST['email'];
 				$msg = $_POST['msg'];
+				$displayName = $name;
+				$displayEmail = $email;
+				$name = str_replace("_", "-v-", $name);
 				$name = str_replace(" ", "_", $name);
+				$name = str_replace("(", "-ciop-",$name);
+				$name = str_replace(")", "-cicl-", $name);
 
-				if(exec("java -cp /var/www/html/kamuri.al/html Mail $email $name $msg", $output)){
-					echo "<font color=\"green\"><br>".htmlentities($name).", your message was sent successfully! 
-					<br>Wait for reply at ".htmlentities($email)."!</font>";	
+				$email = str_replace("_", "-v-",$email);
+				$email = str_replace(" ", "_", $email);
+				$email = str_replace("(","-ciop-", $email);
+				$email = str_replace(")", "-cicl-", $email);
+
+				$msg = str_replace("_","-v-",$msg);
+				$msg = str_replace(" ", "_", $msg);
+				$msg = str_replace("(","-ciop-",$msg);
+				$msg = str_replace(")","-cicl-",  $msg);
+
+				if(exec("java -cp /var/www/html/kamuri.al/mailer/toAdmins Mail $email $name $msg", $output))
+					echo "<font color=\"green\"><br>".htmlentities($displayName).", your message was sent successfully!
+					<br>Wait for reply at ".htmlentities($displayEmail)."!</font>";
+				else{
+					echo "<font color=\"red\">Sorry! We encountered a problem!.</font>";
 				}
-				else
-					echo "<font color=\"red\">Sorry! We encountered a problem! Please retry!</font>";
-				
+
 				$_POST['name'] = "";
 				$_POST['email'] = "";
 				$_POST['msg'] = "";
@@ -25,7 +40,7 @@
 				echo "<font color=\"red\">All fields must be filled!</font>";
 		}
 		else{
-			echo "<font color=\"orange\">Waiting...</font>";
+			echo "<font color=\"orange\">Waiting... Fill the fields! <br>Any special character like #$%^&* will be ignored!</font>";
 		}
 	?>
 	<br>
--- html/login.php
+++ html/login.php
@@ -1,6 +1,6 @@
 <?php
     $lastpage = $_SERVER['HTTP_REFERER'];
-    if (!file_exists('encryptor.php')) die("Something went wrong or signup page missing! 
+    if (!file_exists('encryptor.php')) die("Something went wrong or signup page missing!
 <br>Notify the admins <a href=\"contactMe.php\">here</a> if the problem still exists even after refresh!<br>");
 require_once("encryptor.php");
 
@@ -26,9 +26,9 @@ require_once("encryptor.php");
                 if($conn = mysqli_connect("localhost", "root", "Asdf!234","myDBs")){
                     $email = mysql_escape_string(strtolower(trim($_POST['email'])));
                     $pw = mysql_escape_string($_POST['pw']);
-                    
+
                     $res = $conn->query("SELECT id,email,pw,kryp FROM kamuriTBL WHERE email='$email';");
-                    $numRows = mysqli_num_rows($res); 
+                    $numRows = mysqli_num_rows($res);
                     if($numRows<=0){
                         if (substr($lastpage, strrpos($lastpage, "/")+1) == "index.php")
                           echo "Invalid email/password";
@@ -49,6 +49,7 @@ require_once("encryptor.php");
                             $_SESSION['allowed'] = $tmp['id'];
                             $res->free();
                             $conn->query("UPDATE kamuriTBL SET lastLoginIP='".$ip."' WHERE email='".$email."';");
+                            //here check for status and redirect accordingly
                             if (substr($lastpage, strrpos($lastpage, "/")+1) == "index.php")
                                 header('Location: /kamuri.al/html/home_page.html');
                             else
@@ -62,9 +63,11 @@ require_once("encryptor.php");
                 }
                 else{
                     if (substr($lastpage, strrpos($lastpage, "/")+1) == "index.php")
-                            echo "Couldn't connect to database! Notify the admins <a href=\"contactMe.php\">here</a> if the problem still exists even after refresh!";
+                            echo "Couldn't connect to database! Notify the admins
+                            <a href=\"contactMe.php\">here</a> if the problem still exists even after refresh!";
                         else
-                            echo "Nuk u be lidhja me databazen! Lajmero adminat <a href=\"contactMe.php\">ketu</a> nese ky problem vazhdon edhe pas rifreskimit!";
+                            echo "Nuk u be lidhja me databazen! Lajmero adminat
+                            <a href=\"contactMe.php\">ketu</a> nese ky problem vazhdon edhe pas rifreskimit!";
                 }
             }
         }
